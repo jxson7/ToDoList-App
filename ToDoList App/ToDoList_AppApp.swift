3357cd9703f8708867baf3cdac8361ca2a66a6e4
JASON DOOKARUN <jason.dookarun@live.co.uk>
2022-07-19 15:49:38 +0100

    Initial Commit

---
+++ ToDoList App/ToDoList_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_AppApp.swift
+//  ToDoList App
+//
+//  Created by Jason Dookarun on 19/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
